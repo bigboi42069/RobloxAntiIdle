37dc4d7c2626ee6b264ceacd4c85ea5b4d8a3c02
CSWC <43013100+bigboi42069@users.noreply.github.com>
2024-11-09 19:36:31 -0330

    Fixed AntiIdle.lua
    Fixed.

--- AntiIdle.lua
+++ AntiIdle.lua
@@ -1,6 +1,12 @@
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 
+local function simulateKeyPress()
+    game:GetService("VirtualInputManager"):SendKeyEvent(true, "Z", false, game)
+    wait(0.1)
+    game:GetService("VirtualInputManager"):SendKeyEvent(false, "Z", false, game)
+end
+
 local function teleportPlayer()
     local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
     local currentPosition = humanoidRootPart.Position
@@ -9,29 +15,33 @@ local function teleportPlayer()
     local function setPositionKeepRotation(newPosition)
         humanoidRootPart.CFrame = CFrame.new(newPosition) * currentRotation
     end
-
-    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.1, 0))
+    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.01, 0))
     wait()
-    setPositionKeepRotation(currentPosition + Vector3.new(0.1, 0.1, 0))
+    setPositionKeepRotation(currentPosition + Vector3.new(0.01, 0.01, 0))
     wait()
-    setPositionKeepRotation(currentPosition + Vector3.new(-0.1, 0.1, 0))
+    setPositionKeepRotation(currentPosition + Vector3.new(-0.01, 0.01, 0))
     wait()
-    setPositionKeepRotation(currentPosition + Vector3.new(-0.1, 0.1, 0))
+    setPositionKeepRotation(currentPosition + Vector3.new(-0.01, 0.01, 0))
     wait()
-    setPositionKeepRotation(currentPosition + Vector3.new(0.1, 0.1, 0))
+    setPositionKeepRotation(currentPosition + Vector3.new(0.01, 0.01, 0))
     wait()
-    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.1, 0.1))
+    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.01, 0.01))
     wait()
-    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.1, -0.1))
+    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.01, -0.01))
     wait()
-    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.1, -0.1))
+    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.01, -0.01))
     wait()
-    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.1, 0.1))
-    wait(0.01)
+    setPositionKeepRotation(currentPosition + Vector3.new(0, 0.01, 0.01))
+    wait(0.05)
     setPositionKeepRotation(origPosition)
 end
 
 while true do
-    teleportPlayer()
-    wait(300)
+    local RNG = Random.new()
+    if RNG:NextInteger(1, 20) == 1 then
+        simulateKeyPress()
+        wait()
+        teleportPlayer()
+    end
+    wait(3)
 end
